f76d390273baeca9369d331d2e6adc6522b9a0c3
Julie Kovalenko <juli27july2005@gmail.com>
2025-09-24 14:42:43 +0200

    Initial state

--- js/build.js
+++ js/build.js
@@ -1,10 +1,24 @@
-Fliplet.Widget.instance('publishing', function(data) {
-  var $container = $(this);
-  var widgetId = Fliplet.Widget.getDefaultId() || data.id || 'publishing-' + Date.now();
+// Include your namespaced libraries
+var mySampleCoreLibrary = new Fliplet.Registry.get('publishing:1.0:core');
 
-  $container.html('<p>Publishing widget loaded</p>');
+// This function will run for each instance found in the page
+Fliplet.Widget.instance('publishing-1-0-0', function (data) {
+  // The HTML element for each instance. You can use $(element) to use jQuery functions on it
+  var element = this;
 
-  return {
-    widgetId: widgetId
-  };
+  // Sample implementation to initialise the widget
+  var foo = new mySampleCoreLibrary(element, data);
+
+  // Handle form submission to show alert
+  $(element).find('.form-alert').on('submit', function(e) {
+    e.preventDefault();
+
+    var userInput = $(this).find('#userInput').val();
+
+    if (userInput.trim()) {
+      alert(userInput);
+    } else {
+      alert('Please enter some text!');
+    }
+  });
 });
